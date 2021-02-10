1304ccff909471d85a78a8bd01143b495c22cf7b
vaibhav89000 <vaibhav89000@gmail.com>
2021-02-10 23:48:39 +0530

    fix of err in isauth middleware

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -14,8 +14,9 @@ module.exports = (req,res,next) => {
     try{
         decodedToken = jwt.verify(token, 'somesupersecretsecret');
     } catch {
-        err.statusCode = 500;
-        throw err;
+        const error = new Error('Not authenticated.');
+        error.statusCode = 500;
+        throw error;
     }
     
     if(!decodedToken){
