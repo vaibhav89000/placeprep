af72f51ad23a73b4332e64e685ece0318f5c51fa
vaibhav89000 <vaibhav89000@gmail.com>
2021-02-10 23:46:56 +0530

    nodemailer

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,76 +2,104 @@ const { validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const environment = require('../environment');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+let transporter = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key: environment.API_Url
+  }
+}));
+
+
 const User = require('../models/user');
 
-exports.signup = (req,res,next) => {
- 
-const errors = validationResult(req);
-if (!errors.isEmpty()) {
+exports.signup = (req, res, next) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
     const error = new Error('Validation failed.');
     error.statusCode = 422;
     error.data = errors.array();
     throw error;
-}
+  }
 
   const email = req.body.email;
   const name = req.body.name;
   const password = req.body.password;
   bcrypt
-  .hash(password,12)
-  .then(hashedPw => {
-    const user = new User({
+    .hash(password, 12)
+    .then(hashedPw => {
+      const user = new User({
         email: email,
         password: hashedPw,
         name: name
       });
       return user.save();
-  })
-  .then(result => {
-    res.status(201).json({ message: 'User created!', userId: result._id });
-  })
-  .catch(err => {
-    if (!err.statusCode) {
-      err.statusCode = 500;
-    }
-    next(err);
-  });
-}
-
-exports.login = (req,res,next) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    let loadedUser;
-    User.findOne({ email: email})
-    .then(user => {
-        if(!user){
-            const error = new Error('A user with this email could not be found.');
-            error.statusCode = 401;
-            throw error;
-        }
-        loadedUser = user;
-        return bcrypt.compare(password,user.password);
     })
-    .then(isEqual => {
-        if (!isEqual) {
-            const error = new Error('Wrong password!');
-            error.statusCode = 401;
-            throw error;
+    .then(result => {
+
+      let mailOptions = {
+        from: 'vaibhav89000sharma@gmail.com',  // single sender verified email id
+        to: email, // To whom you want to send address
+        subject: 'Signup confirmattion',
+        text: 'Thanks for signing up on placeprep. Lets share our experience'
+      };
+
+      transporter.sendMail(mailOptions)
+        .then((result) => {
+          res.status(201).json({ message: 'User created!', userId: result._id });
+        })
+        .catch(err => {
+          if (!err.statusCode) {
+            err.statusCode = 500;
           }
-          const token = jwt.sign(
-              {
-                  email: loadedUser.email,
-                  userId: loadedUser._id.toString()
-              },
-              'somesupersecretsecret',
-              { expiresIn: '1h' }
-          );
-          res.status(200).json({ token: token, userId: loadedUser._id.toString(), email: loadedUser.email});
+          next(err);
+        });
+
     })
     .catch(err => {
-        if(!err.statusCode){
-            err.statusCode = 500;
-        }
-        next(err);
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+}
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        const error = new Error('A user with this email could not be found.');
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then(isEqual => {
+      if (!isEqual) {
+        const error = new Error('Wrong password!');
+        error.statusCode = 401;
+        throw error;
+      }
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString()
+        },
+        'somesupersecretsecret',
+        { expiresIn: '1h' }
+      );
+      res.status(200).json({ token: token, userId: loadedUser._id.toString(), email: loadedUser.email });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
     });
 };
